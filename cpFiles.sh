c397e2e8a414bd678ddd9d849655aa1a57cad3b1
Hongwei Qin <glqinhongwei@gmail.com>
2021-02-06 13:45:59 +0800

    Adjust layout.
    Signed-off-by: Hongwei Qin <glqinhongwei@gmail.com>

--- cpFiles.sh
+++ cpFiles.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-path=$1
+path=kernel_patch
 kernelSource=/usr/src/kernels/linux
 files=`ls $path`
 
 for filename in $files
 do
-	if [[ $filename != cpFiles.sh && $filename != qblke ]]
-	then
-		echo cp -r $filename $kernelSource/
-		yes | cp -r $filename $kernelSource/ 2>/dev/null
-	fi
+	echo cp -r $filename $kernelSource/
+	yes | cp -r $filename $kernelSource/ 2>/dev/null
 done
