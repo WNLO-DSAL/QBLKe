bf7b0d2a6a86d13ef7a10ef4f658fee7b7707ff8
Hongwei Qin <glqinhongwei@gmail.com>
2021-04-24 16:46:09 +0800

    Automatically detect nr_luns.
    Signed-off-by: Hongwei Qin <glqinhongwei@gmail.com>

--- qblke/install.sh
+++ qblke/install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+nrchs=$(nvme lnvm id-ns /dev/nvme0n1 | grep "^ chnls" | awk '{print $3}')
+nrluns=$(nvme lnvm id-ns /dev/nvme0n1 | grep "^ luns" | awk '{print $3}')
+endlun=`echo ${nrchs}" * "${nrluns}" - 1" | bc`
+
 # By default, use none scheduler for lightNVM.
 if [[ $1 != "mq" ]];
 then
@@ -7,4 +11,4 @@ then
 fi
 
 insmod qblk.ko
-nvme lnvm create -d nvme0n1 -b 0 -e 127 -n qblkdev -t qblk
+nvme lnvm create -d nvme0n1 -b 0 -e ${endlun} -n qblkdev -t qblk
